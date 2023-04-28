0db00aaadeef7a0cf85353fac62a8162a066f2ac
Rodri2001 <rodrigodamianmartinez2001@gmail.com>
2023-04-28 18:13:40 -0300

    CodigoDePractica

---
+++ CodigoDePractica.js
@@ -0,0 +1,31 @@
+/*
+¡Por supuesto! Aquí te dejo un ejercicio sencillo de JavaScript para que resuelvas:
+Crea una función llamada "sumarNumeros" que tome dos parámetros numéricos y devuelva
+la suma de esos dos números. Luego, llama a la función con dos números de tu elección e imprime el resultado en la consola.
+Puedes comenzar escribiendo el esqueleto de la función y luego llenarlo con el código necesario para hacer la suma y devolver
+el resultado. ¡Buena suerte!¡Por supuesto! Aquí te dejo un ejercicio sencillo de JavaScript para que resuelvas:
+Crea una función llamada "sumarNumeros" que tome dos parámetros numéricos y devuelva la suma de esos dos números.
+Luego, llama a la función con dos números de tu elección e imprime el resultado en la consola.
+Puedes comenzar escribiendo el esqueleto de la función y luego llenarlo con el código necesario para hacer la suma y devolver el resultado.
+¡Buena suerte!
+ */
+
+function sumarNumeros (x , y){
+return x + y
+}
+const resultado = sumarNumeros(3, 5)
+console.log(resultado)
+
+/*
+ * Complete the 'simpleArraySum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY ar as parameter.
+ */
+
+function simpleArraySum(ar) {
+    // Write your code here
+
+
+
+}
